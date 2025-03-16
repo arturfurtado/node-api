3ce2884d29dda15022a659afbbb42d3018857ab3
arturfurtado <artururtado@gmail.com>
2025-03-16 00:46:38 -0300

    test: test signupcontroller

---
+++ src/presentation/controllers/signUpController/index.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '.'
+
+describe('SignUp Controller', () => {
+  test('Should return status code 400 if name doesnt existe in the body request', () => {
+    const sut = new SignUpController()
+    const HttpRequest = {
+      body: {
+        email: 'mail@mail.com',
+        password: 'password',
+        passwordConfirm: 'password'
+      }
+    }
+    const httpResponse = sut.handle(HttpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})

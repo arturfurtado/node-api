458ce1314fbf72d44d4075d9261b55ca772df3c6
arturfurtado <artururtado@gmail.com>
2025-03-16 01:08:20 -0300

    test: returning 400 if email isnt provided

--- src/presentation/controllers/signUpController/index.spec.ts
+++ src/presentation/controllers/signUpController/index.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '.'
 
 describe('SignUp Controller', () => {
-  test('Should return status code 400 if name doesnt existe in the body request', () => {
+  test('Should return status code 400 if name doesnt exists in body request', () => {
     const sut = new SignUpController()
     const HttpRequest = {
       body: {
@@ -13,5 +13,21 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(HttpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing parameter: name'))
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return status code 400 if email doesnt exists in body request', () => {
+    const sut = new SignUpController()
+    const HttpRequest = {
+      body: {
+        name: 'name',
+        password: 'password',
+        passwordConfirm: 'password'
+      }
+    }
+    const httpResponse = sut.handle(HttpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing parameter: email'))
   })
 })
